28cd75aff3dd6e1e5d2f739ad48fb26f9f5d19a9
LP-React <ljamirp30@gmail.com>
2026-01-03 22:02:58 -0500

    fix: endpoint productos

--- src/main/java/com/meevent/webapi/Controller/ProductController.java
+++ src/main/java/com/meevent/webapi/Controller/ProductController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/products")
+@RequestMapping("/api/v2/products")
 public class ProductController {
 
     @Autowired
