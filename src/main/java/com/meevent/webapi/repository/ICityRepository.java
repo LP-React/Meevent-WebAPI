0a095496e004c4bd60164bb8baed5ad8700de55b
Gabriel <i202332157@cibertec.edu.pe>
2026-01-20 22:59:26 -0500

    feat(attendee): add update profile attendee

--- src/main/java/com/meevent/webapi/repository/ICityRepository.java
+++ src/main/java/com/meevent/webapi/repository/ICityRepository.java
@@ -2,6 +2,8 @@ package com.meevent.webapi.repository;
 
 import com.meevent.webapi.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ICityRepository extends JpaRepository<City,Long> {
 }
