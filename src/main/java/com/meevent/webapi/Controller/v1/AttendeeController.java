087649751126641e3a8a600b2248a5e7d4a805c2
Gabriel <i202332157@cibertec.edu.pe>
2026-01-20 20:07:27 -0500

    refactor(attendee): endpoint patch

--- src/main/java/com/meevent/webapi/Controller/v1/AttendeeController.java
+++ src/main/java/com/meevent/webapi/Controller/v1/AttendeeController.java
@@ -9,10 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/attendees")
@@ -21,7 +18,7 @@ public class AttendeeController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AttendeeController.class);
     private final AttendeeService attendeeService;
 
-    @PutMapping("/profile")
+    @PatchMapping("/profile")
     public ResponseEntity<AttendeeProfileResponse> updateMyProfile(
             @Valid @RequestBody UpdateAttendeeProfileRequest request
     ) {
